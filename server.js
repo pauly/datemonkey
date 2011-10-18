af18f04379321e75ad5412ab9221c3c856b4be39
Paul Clarke <pauly@clarkeology.com>
2011-10-18 04:13:24 +0100

    Offer multiple choices more similar to the correct answer where possibl

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var title = 'Date monkey',
   express = require('express'),
   app = module.exports = express.createServer();
 
-// Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -49,16 +47,40 @@ var input = function ( c, q ) {
   return multipleAnswersOneCategory( c, q );
 };
 
+var like = function ( a, b, c ) {
+  if ( a[0] != b[0] ) {
+    if ( c ) {
+      if (( typeof( a[1] ) == 'number' ) && ( typeof( b[1] ) == 'number' )) {
+        // console.log( a[1] + ' - ' + b[1] + ' is ' + ( a[1] - b[1] ) + ' is that less than ' + c );
+        return Math.abs( a[1] - b[1] ) <= c;
+      }
+      else {
+        // console.log( a[0].substr(0,c) + ' == ' + b[0].substr(0,c) + '?' );
+        return a[0].toLowerCase().substr(0,c) == b[0].toLowerCase().substr(0,c);
+      }
+    }
+    return true;
+  }
+};
+
 // Generate a multiple choice input for a question, in a category
 var multipleAnswersOneCategory = function ( c, q ) {
   var r = '';
   var all = shuffle( data[c][1].slice( 0 ));
   var answers = [ data[c][1][q] ];
-  for ( i in all ) {
-    if (( all[i][0] != data[c][1][q][0] ) && ( answers.length < multipleChoices )) {
+  for ( i in all ) { // try and get multiple choices within 3 years
+    if ( like( all[i], data[c][1][q], 3 ) && ( answers.length < multipleChoices )) {
       answers.push( all[i] );
     }
   }
+  if ( answers.length < multipleChoices ) { // let's have another go
+    for ( i in all ) {
+      if ( like( all[i], data[c][1][q] ) && ( answers.length < multipleChoices )) {
+        answers.push( all[i] );
+      }
+    }
+  }
+  // console.log( answers );
   for ( i in shuffle( answers )) {
     r += '<button name="answer" type="submit" value="' + answers[i][0] + '"><span>' + answers[i][0] + '</span></button> ';
   }
@@ -87,7 +109,7 @@ var question = function ( req, res ) {
   if ( ! data[c][1][q] ) {
     q = Math.floor( Math.random( ) * data[c][1].length );
   }
-  r += '<form><input type="hidden" name="taken" value="' + ( req.query.taken || 0 ) + '" /><input type="hidden" name="correct" value="' + ( req.query.correct || 0 ) + '" /><input type="hidden" name="c" value="' + c + '" /><input type="hidden" name="q" value="' + q + '" />';
+  r += '<form><input type="hidden" name="taken" value="' + ( req.query.taken ||  0 ) + '" /><input type="hidden" name="correct" value="' + ( req.query.correct ||  0 ) + '" /><input type="hidden" name="c" value="' + c + '" /><input type="hidden" name="q" value="' + q + '" />';
   r += data[c][0] + ' ' + data[c][1][q][1] + '? ' + input( c, q );
   r += '</form>';
   return r;
@@ -243,6 +265,216 @@ var data = [
       [ "Jim Laker", 1956, "cricket" ],
       [ "Gordon Pirie", 1955, "athletics" ],
       [ "Chris Chataway", 1954, "athletics" ]
+    ]
+  ],
+  [
+    "UK and Ireland IATA codes",
+    [
+[ "Cork", "ORK" ],                                                   
+ [ "Donegal (Carrickfin)", "CFN" ],                                   
+ [ "Dublin", "DUB" ],                                                 
+ [ "Galway", "GWY" ],                                                 
+ [ "Kerry County", "KIR" ],                                           
+ [ "Knock", "NOC" ],                                                  
+ [ "Shannon (Limerick)", "SNN" ],                                     
+ [ "Sligo", "SXL" ],                                                  
+ [ "Bantry Aerodrome", "BYT" ],                                       
+ [ "Belmullet Aerodrome", "BLY" ],                                    
+ [ "Connemara Regional Airport", "NNR" ],                             
+ [ "Castlebar Airport", "CLB" ],                                      
+ [ "Castlebridge Airport", "WEX" ],                                   
+ [ "Cork Airport", "ORK" ],                                           
+ [ "Galway Airport", "GWY" ],                                         
+ [ "Donegal Airport", "CFN" ],                                        
+ [ "Dublin Airport", "DUB" ],                                         
+ [ "Inishmore Aerodrome", "IOR" ],                                    
+ [ "Inisheer Aerodrome", "INQ" ],                                     
+ [ "Kilkenny Airport", "KKY" ],                                       
+ [ "Ireland West Knock Airport", "NOC" ],                             
+ [ "Kerry Airport", "KIR" ],                                          
+ [ "Letterkenny Airport", "LTR" ],                                    
+ [ "Inishmaan Aerodrome", "IIA" ],                                    
+ [ "Shannon Airport", "SNN" ],                                        
+ [ "Sligo Airport", "SXL" ],                                          
+ [ "Waterford Airport", "WAT" ],                                      
+ [ "Kilronan Airport", "IOR" ],                                       
+ [ "Reeroe Airport", "CHE" ],                                         
+ [ "Aberdeen", "ABZ" ],                                               
+ [ "Barra (the famous tidal beach landing)", "BRR" ],                 
+ [ "Belfast - Harbour", "BHD" ],                                      
+ [ "Belfast International", "BFS" ],                                  
+ [ "Benbecula", "BEB" ],                                              
+ [ "Birmingham", "BHX" ],                                             
+ [ "Blackpool", "BLK" ],                                              
+ [ "Bournemouth", "BOH" ],                                            
+ [ "Bristol", "BRS" ],                                                
+ [ "Cambrigde", "CBG" ],                                              
+ [ "Campbeltown", "CAL" ],                                            
+ [ "Cardiff International Airport", "CWL" ],                          
+ [ "Coventry - Baginton", "CVT" ],                                    
+ [ "Derry (Londonderry)", "LDY" ],                                    
+ [ "Doncaster, Robin Hood International Airport", "DCS" ],            
+ [ "Dundee", "DND" ],                                                 
+ [ "Exeter", "EXT" ],                                                 
+ [ "Fair Isle (Shetland)", "FIE" ],                                   
+ [ "Foula (Shetland)", "FOU" ],                                       
+ [ "Glasgow", "GLA" ],                                                
+ [ "Glasgow, Prestwick", "PIK" ],                                     
+ [ "Humberside", "HUY" ],                                             
+ [ "Inverness", "INV" ],                                              
+ [ "Islay", "ILY" ],                                                  
+ [ "Kent (Manston) Kent International", "MSE" ],                      
+ [ "Kirkwall (Orkney)", "KOI" ],                                      
+ [ "Land's End", "LQE" ],                                             
+ [ "Leeds/Bradford", "LBA" ],                                         
+ [ "Lerwick/Tingwall (Shetland Islands)", "LWK" ],                    
+ [ "Liverpool", "LPL" ],                                              
+ [ "London - City Airport", "LCY" ],                                  
+ [ "London - Gatwick", "LGW" ],                                       
+ [ "London - Heathrow", "LHR" ],                                      
+ [ "London - Luton", "LTN" ],                                         
+ [ "London - Stansted", "STN" ],                                      
+ [ "London Metropolitan Area", "LON" ],                               
+ [ "Londonderry - Eglinton", "LDY" ],                                 
+ [ "Manchester", "MAN" ],                                             
+ [ "Newcastle", "NCL" ],                                              
+ [ "Newquay", "NQY" ],                                                
+ [ "Norwich", "NWI" ],                                                
+ [ "Nottingham - East Midlands", "EMA" ],                             
+ [ "Orkney - Kirkwall", "KOI" ],                                      
+ [ "Out Skerries (Shetland)", "OUK" ],                                
+ [ "Sheffield, City Airport", "SZD" ],                                
+ [ "Sheffield, Doncaster, Robin Hood International Airport", "DCS" ], 
+ [ "Southampton - Eastleigh", "SOU" ],                                
+ [ "Southend", "SEN" ],                                               
+ [ "Stansted (London)", "STN" ],                                      
+ [ "Stornway", "SYY" ],                                               
+ [ "Sumburgh (Shetland)", "LSI" ],                                    
+ [ "Teesside, Durham Tees Valley", "MME" ],                           
+ [ "Unst (Shetland Island) - Baltasound Airport", "UNT" ],            
+ [ "Wick", "WIC" ],                                                   
+ [ "Belfast International Airport", "BFS" ],                          
+ [ "St Angelo Airport", "ENK" ],                                      
+ [ "George Best Belfast City Airport", "BHD" ],                       
+ [ "City of Derry Airport", "LDY" ],                                  
+ [ "Birmingham International Airport", "BHX" ],                       
+ [ "Coventry Airport", "CVT" ],                                       
+ [ "Gloucestershire Airport", "GLO" ],                                
+ [ "Sywell Aerodrome", "ORM" ],                                       
+ [ "Nottingham Airport", "NQT" ],                                     
+ [ "Manchester Airport", "MAN" ],                                     
+ [ "Manchester Woodford Airport", "XXB" ],                            
+ [ "Robin Hood Doncaster Sheffield Airport", "DSA" ],                 
+ [ "RAF Lyneham", "LYE" ],                                            
+ [ "MOD St. Athan", "DGX" ],                                          
+ [ "RNAS Yeovilton", "YEO" ],                                         
+ [ "Campbeltown Airport", "CAL" ],                                    
+ [ "Eday Airport", "EOI" ],                                           
+ [ "Fair Isle Airport", "FIE" ],                                      
+ [ "Whalsay Airport", "WHS" ],                                        
+ [ "Coll Airport", "COL" ],                                           
+ [ "North Ronaldsay Airport", "NRL" ],                                
+ [ "Oban Airport", "OBN" ],                                           
+ [ "Papa Westray Airport", "PPW" ],                                   
+ [ "Stronsay Airport", "SOY" ],                                       
+ [ "Sanday Airport", "NDY" ],                                         
+ [ "Lerwick / Tingwall Airport", "LWK" ],                             
+ [ "Westray Airport", "WRY" ],                                        
+ [ "Colonsay Airstrip", "CSA" ],                                      
+ [ "Haverfordwest Airport", "HAW" ],                                  
+ [ "Cardiff International Airport", "CWL" ],                          
+ [ "Swansea Airport", "SWS" ],                                        
+ [ "Bristol International Airport", "BRS" ],                          
+ [ "Liverpool John Lennon Airport", "LPL" ],                          
+ [ "London Luton Airport", "LTN" ],                                   
+ [ "Land's End Airport", "LEQ" ],                                     
+ [ "Plymouth City Airport", "PLH" ],                                  
+ [ "St. Mary's Airport", "ISC" ],                                     
+ [ "Bournemouth Airport", "BOH" ],                                    
+ [ "Southampton Airport", "SOU" ],                                    
+ [ "Bembridge Airport", "BBP" ],                                      
+ [ "Penzance Heliport", "PZE" ],                                      
+ [ "Lasham Airport", "QLA" ],                                         
+ [ "Newquay Cornwall Airport", "NQY" ],                               
+ [ "Chichester/Goodwood Airport", "QUG" ],                            
+ [ "Shoreham Airport", "ESH" ],                                       
+ [ "London Biggin Hill Airport", "BQH" ],                             
+ [ "London Gatwick Airport", "LGW" ],                                 
+ [ "Redhill Aerodrome", "KRH" ],                                      
+ [ "London City Airport", "LCY" ],                                    
+ [ "Farnborough Airport", "FAB" ],                                    
+ [ "Blackbushe Airport", "BBS" ],                                     
+ [ "London Heathrow Airport", "LHR" ],                                
+ [ "Southend Airport", "SEN" ],                                       
+ [ "Lydd Airport", "LYX" ],                                           
+ [ "Kent International Airport", "MSE" ],                             
+ [ "Carlisle Airport", "CAX" ],                                       
+ [ "Blackpool International Airport", "BLK" ],                        
+ [ "Humberside Airport", "HUY" ],                                     
+ [ "Barrow Walney Island Airport", "BWF" ],                           
+ [ "Leeds Bradford Airport", "LBA" ],                                 
+ [ "Hawarden Airport", "CEG" ],                                       
+ [ "Newcastle Airport", "NCL" ],                                      
+ [ "Durham Tees Valley Airport", "MME" ],                             
+ [ "East Midlands Airport", "EMA" ],                                  
+ [ "Anglesey Airport", "HLY" ],                                       
+ [ "Kirkwall Airport", "KOI" ],                                       
+ [ "Sumburgh Airport", "LSI" ],                                       
+ [ "Wick Airport", "WIC" ],                                           
+ [ "Inverness Airport", "INV" ],                                      
+ [ "Glasgow International Airport", "GLA" ],                          
+ [ "Edinburgh Airport", "EDI" ],                                      
+ [ "Islay Airport", "ILY" ],                                          
+ [ "Glasgow Prestwick Airport", "PIK" ],                              
+ [ "Benbecula Airport", "BEB" ],                                      
+ [ "Scatsta Airport", "SCS" ],                                        
+ [ "Dundee Airport", "DND" ],                                         
+ [ "Stornoway Airport", "SYY" ],                                      
+ [ "Barra Airport", "BRR" ],                                          
+ [ "Perth/Scone Airport", "PSL" ],                                    
+ [ "Tiree Airport", "TRE" ],                                          
+ [ "Unst Airport", "UNT" ],                                           
+ [ "Ballykelly Airport", "BOL" ],                                     
+ [ "RAF Kinloss", "FSS" ],                                            
+ [ "RAF Leuchars", "ADX" ],                                           
+ [ "RAF Lossiemouth", "LMO" ],                                        
+ [ "Cambridge Airport", "CBG" ],                                      
+ [ "Norwich International Airport", "NWI" ],                          
+ [ "London Stansted Airport", "STN" ],                                
+ [ "Duxford Airport", "QFO" ],                                        
+ [ "Sheffield City Heliport", "SZE" ],                                
+ [ "Exeter International Airport", "EXT" ],                           
+ [ "Fairoaks Airport", "FRK" ],                                       
+ [ "Bristol Filton Airport", "FZO" ],                                 
+ [ "Oxford (Kidlington) Airport", "OXF" ],                            
+ [ "Rochester Airport", "RCS" ],                                      
+ [ "Elstree Airfield", "ETR" ],                                       
+ [ "RAF Benson", "BEX" ],                                             
+ [ "RAF Abingdon", "ABB" ],                                           
+ [ "RAF Lakenheath", "LKZ" ],                                         
+ [ "RAF Mildenhall", "MHZ" ],                                         
+ [ "RAF Wyton", "QUY" ],                                              
+ [ "RAF Fairford", "FFD" ],                                           
+ [ "RAF Brize Norton", "BZZ" ],                                       
+ [ "RAF Odiham", "ODH" ],                                             
+ [ "Wethersfield Airport", "WXF" ],                                   
+ [ "Andover Airfield", "ADV" ],                                       
+ [ "RAF Northolt", "NHT" ],                                           
+ [ "Binbrook Airfield", "GSY" ],                                      
+ [ "RAF Coningsby", "QCY" ],                                          
+ [ "RAF Honington", "BEQ" ],                                          
+ [ "RAF Cottesmore", "OKH" ],                                         
+ [ "RAF Scampton", "SQZ" ],                                           
+ [ "RAF Linton-On-Ouse", "HRT" ],                                     
+ [ "RAF Waddington", "WTN" ],                                         
+ [ "RAF Marham", "MRH" ],                                             
+ [ "Dornoch Airport", "DOC" ],                                        
+ [ "Flotta Isle Airport", "FLH" ],                                    
+ [ "Foula Airport", "FOA" ],                                          
+ [ "Outer Skerries Airport", "OUK" ],                                 
+ [ "Papa Stour Airport", "PSV" ],                                     
+ [ "Glenforsa Airfield", "ULL" ],                                     
+      [ "Skye Bridge Ashaig Airport", "SKL" ]
     ]
   ]
 ];
