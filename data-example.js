43a6277fc959ae6d92fc6d81b3ecc78bc1389322
Paul Clarke <pauly@clarkeology.com>
2012-02-27 16:20:37 +0000

    moved data out to a file of its own

---
+++ data-example.js
@@ -0,0 +1,30 @@
+// All the data in a big array, individual answers should be [ "Answer", year, "optional trivia (unused so far)" ]
+module.exports = [
+  [
+    'Best picture',
+    [
+      [ "King's Speech", 2010 ],
+      [ "Hurt Locker", 2009 ],
+      [ "Slumdog Millionaire", 2008 ]
+    ],
+    'Not much data here...'
+  ],
+  [
+    'Best actor',
+    [
+      [ "Colin Firth", 2010, "King's Speech" ],
+      [ "Jeff Bridges", 2009, "Crazy Heart" ],
+      [ "Sean Penn", 2008, "Milk" ]
+    ],
+    'Not much data here...'
+  ],
+  [
+    'Best actress',
+    [
+      [ "Natalie Portman", 2010, "Black Swan" ],
+      [ "Sandra Bullock", 2009, "The Blind Side" ],
+      [ "Kate Winslet", 2008, "The Reader" ]
+    ],
+    'Need to add more actresses here I know, coming soon...'
+  ]
+];
