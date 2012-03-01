d31863c2dff20ea64ae626c5e0f8c8f0dacaed24
Paul Clarke <pauly@clarkeology.com>
2012-03-01 09:59:51 +0000

    fixed express issues

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ io.sockets.on( 'connection', function ( socket ) {
   } );
 } );
 
-/* app.configure(function(){
+app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
+  // app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-}); */
+  // app.use(express.static(__dirname + '/public'));
+});
 
 app.configure( 'production', function( ) {
   app.use( express.errorHandler( ));
