24a8032854ab3baaa1a12887851485f9cacd67ff
Paul Clarke <pauly@clarkeology.com>
2014-05-14 12:40:28 +0100

    back to 12248

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.all( '/random', function( req, res ) {
   } );
 } );
 
-app.listen( 80 );
+app.listen( 12248 );
 // console.log( 'Express server listening on port %d in %s mode', app.address().port, app.settings.env );
 
 // initialise
