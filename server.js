99267dcd458601e4f2261a13b7454d867e6978ba
Paul Clarke <pauly@clarkeology.com>
2014-05-14 12:51:33 +0100

    allow for heroku dynamic port

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.all( '/random', function( req, res ) {
   } );
 } );
 
-app.listen( 12248 );
+app.listen( process.env.PORT || 12248 );
 // console.log( 'Express server listening on port %d in %s mode', app.address().port, app.settings.env );
 
 // initialise
